516ed31546c6519138582afc9345e43c7936d641
smano-20052002 <smano4570@gmail.com>
2024-05-18 09:38:12 +0530

    Initial

---
+++ LXP/LXP.Api/Controllers/MaterialController.cs
@@ -0,0 +1,44 @@
+﻿using LXP.Core.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LXP.Common.ViewModels;
+
+
+
+using LXP.Common.Constants;
+using System.Net;
+using LXP.Common.Entities;
+namespace LXP.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaterialController : BaseController
+    {
+        private readonly IMaterialServices _materialService;
+        public MaterialController(IMaterialServices materialService)
+        {
+            _materialService = materialService;
+        }
+
+        [HttpPost("/lxp/course/material")]
+        public async Task<IActionResult> AddMaterial([FromForm]MaterialViewModel material)
+        {
+            MaterialListViewModel createdMaterial = await _materialService.AddMaterial(material);
+            if (createdMaterial!=null)
+            {
+                return Ok(CreateInsertResponse(createdMaterial));
+            }
+            else
+            {
+                return Ok(CreateFailureResponse(MessageConstants.MsgAlreadyExists, (int)HttpStatusCode.PreconditionFailed));
+
+            }
+        }
+
+        [HttpGet("/lxp/course/topic/{topicId}/materialtype/{materialTypeId}/")]
+        public async Task<List<MaterialListViewModel>> GetAllMaterialDetailsByTopicAndMaterialType(string topicId,string materialTypeId)
+        {
+            return await _materialService.GetAllMaterialDetailsByTopicAndType(topicId,materialTypeId) ;
+        }
+    }
+}
